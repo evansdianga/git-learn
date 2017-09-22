5275c77ca3783e57c2fad8a87ad43c0365f85f03
George Nyakundi <gee.nyakundi@gmail.com>
2017-09-22 12:51:54 +0300

    Managed to fix, the tickClock function to tick the clock

--- app.js
+++ app.js
@@ -2,4 +2,13 @@
 (function () {
     let content = document.getElementById('message');
     content.innerHTML = 'Evans Dianga';
+
+    setInterval(tickClock,1000);
+
+
+    function tickClock(){
+        let CurrentTime = new Date();
+        //content.innerHTML = CurrentTime.getHours() +" :" + CurrentTime.getMinutes()+" :"+CurrentTime.getMilliseconds *1000;
+        content.innerHTML = CurrentTime;
+    }
 })();
